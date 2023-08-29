917e839c923485e604f1ac88bdac9992ced8fa57
Khare <rajshree.khare@dell.com>
2023-08-29 19:59:07 +0530

    pflx: rename sdc feature added

--- content/docs/deployment/csminstallationwizard/src/static/js/generate-yaml.js
+++ content/docs/deployment/csminstallationwizard/src/static/js/generate-yaml.js
@@ -125,6 +125,9 @@ function setValues(csmMapValues, CONSTANTS_PARAM) {
 	DriverValues.vSphereFCHostName = $("#vSphere-fc-host-name").val();
 	DriverValues.vSphereVCenterHost = $("#vSphere-vCenter-host").val();
 	DriverValues.vSphereVCenterCredSecret = $("#vSphere-vCenter-cred-secret").val();
+
+	DriverValues.renameSDC = $("#renameSDC").prop('checked') ? true : false;
+	DriverValues.snapNamePrefix = document.getElementById("snapshot-prefix").value;
 	return DriverValues
 }
 
